cc327cb708cd8474e2dc19b7c1e66944ce2b288e
nomadic-dev <nomadicdev.io@gmail.com>
2024-09-07 13:19:10 +0400

    lotti json added

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ import { DotLottie } from '@lottiefiles/dotlottie-web';
 
 const dotLottie = new DotLottie({
   canvas: document.querySelector('#logo_anim'),
-  src: './src/json/nomadic-logo-animation.json',
+  src: 'https://app.lottiefiles.com/animation/8f18c756-8969-4615-ab8f-24d594206765',
 });
 
 
