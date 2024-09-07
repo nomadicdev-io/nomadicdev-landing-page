aac5520d76ea9e1a7ad9f708e019220b539e5fce
nomadic-dev <nomadicdev.io@gmail.com>
2024-09-07 12:44:49 +0400

    anim added

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
 "use strict"
+
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+
+const dotLottie = new DotLottie({
+  canvas: document.querySelector('#logo_anim'),
+  src: '/src/json/nomadic-logo-animation.json',
+});
+
+
+window.addEventListener('load', ()=> {
+  document.body.classList.add('dom_loaded')
+  dotLottie.load({autoplay: true})
+  setTimeout(()=> {
+    document.getElementById('loader').remove()
+  }, 1000)
+})
+
